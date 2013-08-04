80eca563d56b17bcead24f838b0e987936e30c8e
tomasfabian <fabian.frameworks@gmail.com>
2013-08-04 10:33:13 +0200

    Custom aspect demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Ninject.Extensions.AspectsWeaver
 =====================
 
-Ninject.Extensions.AspectsWeaver is an extension for Ninject inversion of control container and is based on Castle Dynamic Proxy. The main purpose of this module is to allow aspect oriented programming in the .NET environment. It also allows you to intercept your non virtual member calls. This option is usually not provided by other modules base on dynamic proxies.
+Ninject.Extensions.AspectsWeaver is an extension for Ninject inversion of control container and is based on Castle Dynamic Proxy. The main purpose of this module is to allow aspect oriented programming in the .NET environment. It also allows you to intercept your non virtual member calls. This option is usually not provided by other modules based on dynamic proxies.
 
 For more information see Wiki
---
+++ src/Ninject.Extensions.AspectsWeaver/Examples/CustomAspectsDemo.cs
@@ -0,0 +1,80 @@
+﻿// #region License
+// // 
+// // Author: Tomas Fabian <fabian.frameworks@gmail.com>
+// // Copyright (c) 2013, Tomas Fabian
+// // 
+// // Licensed under the Apache License, Version 2.0.
+// // See the file LICENSE.txt for details.
+// // 
+
+using System;
+using Ninject;
+using Ninject.Extensions.AspectsWeaver.Aspects.Contracts;
+using Ninject.Extensions.AspectsWeaver.Aspects.Custom;
+using Ninject.Extensions.AspectsWeaver.Modules;
+using Ninject.Extensions.AspectsWeaver.Planning.Bindings;
+
+namespace AspectWeaver.Demo.CustomAspects
+{
+    class Program
+    {
+        private static void Main()
+        {
+            InterceptGetterAndSetter();
+        }
+
+        public static void InterceptGetterAndSetter()
+        {
+            Ninject.IKernel kernel = new Ninject.StandardKernel(new AspectsWeaverModule());
+
+            var binding = kernel.Bind<IFoo>().To<Foo>();
+
+            binding
+            .WeaveGetProperty(foo => foo.FooProperty)
+            .Into<LoggingGetPropertyAspect>();
+
+            binding
+            .WeaveSetProperty(foo => foo.FooProperty)
+            .Into<LoggingSetPropertyAspect>();
+
+            var instance = kernel.Get<IFoo>();
+
+            int result = instance.FooProperty;
+            instance.FooProperty = 2;
+        }
+    }
+
+    public interface IFoo
+    {
+        int FooProperty { get; set; }
+    }
+
+    public class Foo : IFoo
+    {
+        public int FooProperty { get; set; }
+    }
+
+    public sealed class LoggingGetPropertyAspect : AfterAspect
+    {
+        protected override void Success(ISuccessArgs args)
+        {
+            base.Success(args);
+
+            Console.WriteLine(string.Format("Log get value: {0}", args.Invocation.ReturnValue));
+        }
+    }
+
+    public sealed class LoggingSetPropertyAspect : AfterAspect
+    {
+        protected override void Success(ISuccessArgs args)
+        {
+            base.Success(args);
+
+            Console.WriteLine(string.Format("Log set value: {0}", args.Invocation.GetArgumentValue(0)));
+        }
+    }
+
+    //Output:
+    //Log get value: 0
+    //Log set value: 2
+}
